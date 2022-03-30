61a42c6b758ec31ebac5da7bc0628311c6b5ef45
+ <+>
2022-03-29 20:27:44 -0400

    completed

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,13 @@
 import React from "react";
 
 function Article({ title, date, preview }) {
+    // const date = "January 1, 1970"
   return (
   
   <article>
-
+      <h3>{title}</h3>
+      {date ? <small>{date}</small> : date = "January 1, 1970"}
+      <p>{preview}</p>
   </article>
   )
 }
